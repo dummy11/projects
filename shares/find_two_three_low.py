0d249ee1894bbfb3c55b3b1eeb58fa6e2d5333ac
feixiaduibing <wangcong127@163.com>
2019-07-17 23:39:16 +0800

    update find three low

--- shares/find_two_three_low.py
+++ shares/find_two_three_low.py
@@ -30,10 +30,11 @@ class find_two_three_low(object):
     def is_three_low(self, data):
         threshold = 0.03
         high_low_threshold = 0.20
-        continue_day = 20
+        continue_day = 30
         day_before = 3
         low = list(data.low)[0:continue_day]
         high = list(data.high)[0:continue_day]
+        real_lowest_value = min(list(data.low))
         highest_value = max(high)
         highest_index = high.index(highest_value)
         lowest_value = min(low)
@@ -43,9 +44,9 @@ class find_two_three_low(object):
         second_lowest_value = min(second_low)
         second_lowest_index = low.index(second_lowest_value)
         index_list = sorted([lowest_index, second_lowest_index])
-        if highest_index >= index_list[0]+3 and highest_index <= index_list[1]-3:
+        if lowest_value <= real_lowest_value*(1+0.15) and highest_index >= index_list[0]+3 and highest_index <= index_list[1]-3 and lowest_index < continue_day-1 and second_lowest_value < continue_day-1:
             if highest_value >= lowest_value*(1+high_low_threshold) and (second_lowest_value <= lowest_value*(1+threshold)):
-                if (low[0] <= lowest_value*(1+threshold)):
+                if (low[0] <= lowest_value*(1+threshold+0.02)):
                     return 1
                 else:
                     return 0
